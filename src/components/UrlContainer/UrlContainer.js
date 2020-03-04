026d5aa57fe1b3c44751b9cae5f3a0f57b532970
Noah Gambee <noah@gambee.dev>
2020-03-04 10:23:31 -0700

    Update componentDidMount function in UrlContainer

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './UrlContainer.css';
 import { connect } from 'react-redux';
+import { setUrls } from '../../actions/index'
+import { getUrls } from '../../apiCalls'
 
 
-export const UrlContainer = (props) => {
+export class UrlContainer extends Component {
+  constructor() {
+    super();
+ }
+
+ componentDidMount() {
+  getUrls()
+    .then(data => data.urls.forEach(url => {
+      this.props.setUrls(url)
+    }))
+    .catch(err => console.error('Error fetching:', err));
+}
+ 
+ render() {
   let urlEls = []
-  if (props.urls) {
-   urlEls = props.urls.map(url => {
+  if (this.props.urls) {
+   urlEls = this.props.urls.map(url => {
     return (
       <div className="url">
         <h3>{url.title}</h3>
@@ -15,17 +30,21 @@ export const UrlContainer = (props) => {
       </div>
     )
   });
-}
-
-  return (
-    <section>
+ }
+    return ( <section>
       { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
     </section>
-  )
+    )
+  }
 }
+
 
 export const mapStateToProps = ({ urls }) => ({
   urls
 });
 
-export default connect(mapStateToProps, null)(UrlContainer)
+export const mapDispatchToProps = dispatch => ({
+  setUrls: (url) => dispatch(setUrls(url))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UrlContainer)
