6a7b114261f47fdeb8d033f1bb93553c4c462a75
Noah Gambee <noah@gambee.dev>
2020-03-04 09:57:01 -0700

    Add tests for urls reducer

---
+++ src/reducers/urls.test.js
@@ -0,0 +1,19 @@
+import { urls } from './urls'
+
+describe('urls reducer tests', () => {
+  it('should return the initial state if no state is provided', () => {
+    const expected = [];
+    const result = urls(undefined, {})
+    expect(result).toEqual(expected)
+  })
+  it('should return the correct state if the action.type is SET_URLS', () => {
+    const mockState = [{longUrl: 'some url #1'}, {longUrl: 'some url #2'} ]
+    const mockAction = {
+      type: 'SET_URLS',
+      url: {longUrl: 'new url #3'}
+    }
+    const expected = [{longUrl: 'some url #1'}, {longUrl: 'some url #2'}, {longUrl: 'new url #3'}]
+    const result = urls(mockState, mockAction)
+    expect(result).toEqual(expected)
+  })
+})
